df09f4aec838704adc28af21b447f1d6304f5f4c
Garibzade <161576671+Garibzade@users.noreply.github.com>
2024-04-23 21:40:52 +0400

    Add files via upload

---
+++ HomeTaskMethodMethodoverload/MinValue/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTaskMethodMethodoverload.MinValue
+{
+    internal static class Class1
+    {
+        public static int MinValue(int[] arr)
+        {
+
+            int min = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+
+                }
+
+            }
+
+            return min;
+        }
+    }
+}
---
+++ HomeTaskMethodMethodoverload/Program.cs
@@ -0,0 +1,33 @@
+﻿using HomeTaskMethodMethodoverload.MinValue;
+
+namespace HomeTaskMethodMethodoverload
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] newArr = new int[] { 1,2,3,4,5,6,7,8,9};
+            Class1.MinValue(newArr);
+
+        }
+        public static int Area(int r)
+        {
+            return 3 * r * r;
+        }
+        public static int Area(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Area(int a, int b, int c)
+        {
+
+            return 2 * (a * b + b * c + c * b);
+        }
+        public static int Area(int a, int b, int c, int r)
+        {
+            int p = (a + b + c) / 2;
+            int S = p * r;
+            return S;
+        }
+    }
+}
